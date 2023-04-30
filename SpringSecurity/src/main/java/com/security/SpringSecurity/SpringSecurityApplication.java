2fa1ced202f92ce17baa80221a1a35782402eeae
ChandrikaNagaraj <ch.nagaraj@gmail.com>
2023-04-30 13:45:11 +0530

    Add files via upload
    Initial Commit

---
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.security.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.security.controller")
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
